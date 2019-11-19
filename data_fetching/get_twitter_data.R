698840f3e50ae88725d2d847dc212641a172128d
Juan Orduz <juanitorduz@gmail.com>
2019-11-19 18:03:17 +0100

    Create get_twitter_data.R

---
+++ data_fetching/get_twitter_data.R
@@ -0,0 +1,39 @@
+twitter_terms <- c(
+  "21N", 
+  "#21N", 
+  "#Paro21N", 
+  "#YoMarchoEste21",
+  "#YoMarchoEl21",
+  "#YoNoMarchoEste21",
+  "#YoNoMarchoEl21",
+  "#RazonesParaMarchar",
+  "#RazonesParaNoMarchar",
+  "#100RazonesParaMarchar", 
+  "#100RazonesParaNoMarchar", 
+  "#YoNoParo",
+  "#YoParoEl21NSinMiedo"
+)
+
+twitter_query <- stringr::str_c(twitter_terms, collapse = " OR ")
+
+raw_df <- search_tweets(
+  q = twitter_query, 
+  n = 2E5, 
+  type = "recent", 
+  include_rts = TRUE, 
+  parse = TRUE, 
+  retryonratelimit = TRUE, 
+  verbose = TRUE
+)
+
+raw_df <- rtweet::search_tweets(
+  q = twitter_query, 
+  n = 2E5, 
+  type = "recent", 
+  include_rts = TRUE, 
+  parse = TRUE, 
+  retryonratelimit = TRUE, 
+  verbose = TRUE
+)
+
+#saveRDS(object = raw_df, file = "19_11_2019_twitter_21n.rds")
