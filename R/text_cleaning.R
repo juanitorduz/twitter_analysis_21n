b10280728c6e34aa26fd1ad14ce13a0792368482
Juan Orduz <juanorduz@TD-Juans-MacBook-Pro.local>
2020-01-30 17:21:28 +0100

    First version text cleaning function. need to ass test!

---
+++ R/text_cleaning.R
@@ -0,0 +1,30 @@
+library(magrittr)
+
+rm_pattern <- tm::content_transformer( 
+  function (x, pattern) stringr::str_remove_all(string = x, pattern = pattern)
+)
+
+clean_tweets <- function(tweet_text) {
+  
+  tweet_text_lwr <- stringr::str_to_lower(tweet_text)
+  
+  tweet_corpus <- tm::VCorpus(x = tm::VectorSource(tweet_text_lwr))
+  
+  url_pattern <- " ?(f|ht)(tp)(s?):(//)?\\S+"
+  
+  clean_tweet_corpus <- tweet_corpus %>% 
+    tm::tm_map(rm_pattern, "/") %>% 
+    tm::tm_map(rm_pattern, "\\|") %>% 
+    tm::tm_map(rm_pattern, url_pattern) %>% 
+    tm::tm_map(tm::stripWhitespace) %>% 
+    tm::tm_map(tm::PlainTextDocument)
+  
+    clean_tweet_text <- sapply(
+      X = clean_tweet_corpus, 
+      FUN = function(x) x$content
+    )
+    
+    clean_tweet_text <- as.vector(clean_tweet_text)
+    
+  return(clean_tweet_text)
+}
