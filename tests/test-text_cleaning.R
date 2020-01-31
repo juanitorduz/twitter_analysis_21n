e68d290facece8ce183c6c1c356f512c9b56e065
Juan Orduz <juanorduz@TD-Juans-MacBook-Pro.local>
2020-01-31 12:08:00 +0100

    add test to impose remove hastag symbol, as the hastags are stored ectracted in another column anyway

--- tests/test-text_cleaning.R
+++ tests/test-text_cleaning.R
@@ -6,9 +6,11 @@ test_that(desc = "Clean Tweet Text", code = {
   clean_text_1 <- clean_tweets(tweet_text = "  ** https://google.com.co juanito.. ..")
   clean_text_2 <- clean_tweets(tweet_text = "!!! tabitas http://google.com.co")
   clean_text_3 <- clean_tweets(tweet_text = "!!!   *+     --    ")
+  clean_text_4 <- clean_tweets(tweet_text = " #elguay")
   expect_equal(clean_text_1, "juanito")
   expect_equal(clean_text_2, "tabitas")
   expect_equal(clean_text_3, "")
+  expect_equal(clean_text_4, "elguay")
 })
 
 
