79eba47f4feb33978c21a07964ee62f47ddd319b
Juan Orduz <juanorduz@TD-Juans-MacBook-Pro.local>
2020-01-31 12:00:20 +0100

    add first version text cleaning function plus test

--- R/text_cleaning.R
+++ R/text_cleaning.R
@@ -4,20 +4,24 @@ rm_pattern <- tm::content_transformer(
   function (x, pattern) stringr::str_remove_all(string = x, pattern = pattern)
 )
 
+trim_both <- tm::content_transformer( 
+  function (x) stringr::str_trim(string = x, side = "both")
+)
+
 clean_tweets <- function(tweet_text) {
   
   tweet_text_lwr <- stringr::str_to_lower(tweet_text)
   
   tweet_corpus <- tm::VCorpus(x = tm::VectorSource(tweet_text_lwr))
   
-  url_pattern <- " ?(f|ht)(tp)(s?):(//)?\\S+"
+  url_pattern <- "(f|ht)(tp)(s?):(//)?\\S+"
   
   clean_tweet_corpus <- tweet_corpus %>% 
     tm::tm_map(rm_pattern, "/") %>% 
     tm::tm_map(rm_pattern, "\\|") %>% 
     tm::tm_map(rm_pattern, url_pattern) %>% 
-    tm::tm_map(tm::stripWhitespace) %>% 
-    tm::tm_map(tm::PlainTextDocument)
+    tm::tm_map(tm::removePunctuation) %>% 
+    tm::tm_map(trim_both)
   
     clean_tweet_text <- sapply(
       X = clean_tweet_corpus, 
