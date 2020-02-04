36beb65f64eafd7f6f34eae0df5a8adba61b3277
juanitorduz <juanitorduz@gmail.com>
2020-02-04 18:56:00 +0100

    add framework to detect topics via community detection on networks

--- R/newtheme.R
+++ R/newtheme.R
@@ -4,4 +4,4 @@ ggmyplot <- function(...){
     scale_fill_brewer(palette = "Set1", aesthetics = c("color", "fill"))
 }
   
-PALETTE_SET1 <- RColorBrewer::brewer.pal(n=9, name = "Set1")
+PALETTE_SET1 <- RColorBrewer::brewer.pal(n = 9, name = "Set1")
